4e99d4a885ff58272334490a9f3e6ee7ffad269c
Theresa Kuipers <abbeyster@gmail.com>
2026-09-24 16:07:09 -0600

    add remote terraform state backend

--- terraform/gcp-gke/providers.tf
+++ terraform/gcp-gke/providers.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "gcs" {
+    bucket = "civic-champion-439320-a5-terraform-state"
+    prefix = "gke"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
